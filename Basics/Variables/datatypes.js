8fa9229d3221d3bb730c6936aa04cc3ebe5b7757
Ken <yash20004x17@gmail.com>
2024-09-08 16:21:43 +0530

    continuing with javascript variables and datatypes are done

---
+++ Basics/Variables/datatypes.js
@@ -0,0 +1,14 @@
+
+//Datatypes
+//number => 2 to power of 53
+//bigint => Not used unless it's a very big number
+//string => can be used with either '' single quotes or "" double quotes
+//boolean => true/false
+//null => standalone value / represents empty value
+//undefined => It means that the variable is declared but the value isn't.
+//symbol => represents unique 
+
+//object => will be discussed further
+
+console.log(typeof null); //output : object
+console.log(typeof undefined);//output : undefined
---
+++ Basics/Variables/variables.js
@@ -0,0 +1,9 @@
+// This is part of the first video const, let and var.
+
+const accountId = 9354938750
+let accountName ="Yash"
+var accountPassword ="@Y2a0s0h4"
+let accountState
+/*Prefer not to use "var" because of issue in block and functional scope */
+console.table([accountId , accountName , accountPassword , accountState])
+
