cbfc885928c10673fb72fdfdb30898e0dc7bb28c
Ken <yash20004x17@gmail.com>
2024-09-10 22:12:21 +0530

    Learning the javascript and on the way to complete the youtube series

---
+++ Basics/Variables/ConversionOperation.js
@@ -0,0 +1,99 @@
+// For number 
+// let score = 18
+
+// console.log(score);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// For null value
+// let score = null
+
+// console.log(score);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//For undefined value
+// let score = undefined
+
+// console.log(score);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// For Boolean value
+// let score = false
+
+// console.log(score);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// For string value
+// let score = "Yash"
+
+// console.log(score);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not an Number)
+// true => 1; false => 0;
+
+// let isLoggedIn = "Yash"
+// console.log(isLoggedIn);
+
+
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""/empty => false
+// "Yash"/ not empty => true
+
+//String value
+// let RandomNumber = 18
+// console.log(typeof RandomNumber);
+// console.log(RandomNumber);
+// let RandomNumberIsString = String(RandomNumber)
+// console.log(typeof RandomNumberIsString);
+// console.log(RandomNumberIsString);
+
+//********************* Operations ***********************
+   
+let value = 3
+let negvalue = -value
+// console.log(value);
+
+// console.log(negvalue);
+
+// //Basic arithmetic operations
+// console.log(10*5);
+// console.log(10-5);
+// console.log(10+5);
+// console.log(10**5);
+// console.log(10/5);
+// console.log(10%3);
+
+//Way to add string.
+// let str1 = "Christ"
+// let str2 = " is "
+// let str3 = "KING"
+
+// let str4 = str1 + str2 + str3
+// console.log(typeof str4);
+// console.log(str4);
+
+console.log("1"+2);
+console.log(1+"2");
+
