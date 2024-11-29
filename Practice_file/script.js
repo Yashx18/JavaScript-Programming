bca9b365556b0d0aed22f2086f04ac8ac6e13697
Ken <yash20004x17@gmail.com>
2024-11-29 22:29:16 +0530

    Files added

---
+++ Practice_file/script.js
@@ -0,0 +1,7 @@
+console.log("Hello User")
+document.getElementById('guts').addEventListener('click', function(){
+    alert("You clicked on the picture of Guts")
+})
+document.getElementById('takezo').addEventListener('click', function(){
+    alert("You clicked on the picture of Shinmen Takezo")
+})
